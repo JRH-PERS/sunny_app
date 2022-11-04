928942089bc3a288dd94791aff2e4a35aa20f9da
JRH-PERS <101594313+JRH-PERS@users.noreply.github.com>
2022-11-04 22:46:25 +0000

    Add files via upload

---
+++ sunny_app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+
+
+# In[7]:
+
+
+header = st.container()
+data = st.container()
+scoreboard = st.container()
+display = st.container()
+
+with header:
+    st.title('The Its Always Sunny Ranker')
+
